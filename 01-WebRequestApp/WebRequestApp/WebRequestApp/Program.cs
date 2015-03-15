0b3768d31f82ae61eadc313b7bee0c3ce3aed38f
tkingless <tkingless@github.com>
2015-03-15 23:10:04 +0800

    init commit

---
+++ 01-WebRequestApp/WebRequestApp/WebRequestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace WebRequestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest req = WebRequest.Create("http://www.microsoft.com");
+            WebResponse resp = req.GetResponse();
+            StreamReader reader = new StreamReader(resp.GetResponseStream(),Encoding.ASCII);
+            //Console.WriteLine(reader.ReadToEnd());
+
+            File.WriteAllText(Directory.GetCurrentDirectory()+"\\example.html",reader.ReadToEnd());
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            Console.ReadLine();
+        }
+    }
+}
